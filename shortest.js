68f2133a291673a057d1dc2d86bc627659dc3369
PatreceCampbell <patrececampbell@mymona.uwi.edu>
2021-11-21 20:57:50 -0500

    Added Progress Bar

--- shortest.js
+++ shortest.js
@@ -4,6 +4,8 @@
         var lastRowNum = parseInt(lastRow.children[0].textContent);
         console.log(lastRowNum)
         var newRow = document.createElement("tr");
+        var startbutton = document.getElementById("StartButton");
+        var progressbar = document.getElementById("Bar");
 
         $(".add-row").click(function(){
             newRow.innerHTML = "<tr><td>"+(lastRowNum+1)+"</td><td><input type='text' name='record'></td><td><input type='text' name='record'></td>";
@@ -20,4 +22,27 @@
                 }
             });
         });
+
+       // Click event for the Start Button
+        startbutton.onclick = function(){
+            barmovement();
+        };
+
+        // This function is responsible for the movement of the progress bar.
+        function barmovement(){
+                var barwidth = 0;
+                var interval = setInterval(progress, 80);
+                
+                function progress(){
+                    if (barwidth >= 100){
+                        clearInterval(interval);
+                    }
+                    else {
+                        barwidth ++;
+                        progressbar.style.width = barwidth + "%";
+                        progressbar.style.backgroundColor = "rgb(70, 132, 248)"
+                        progressbar.innerHTML = barwidth + "%";
+                    }
+                }
+        }
     });    
